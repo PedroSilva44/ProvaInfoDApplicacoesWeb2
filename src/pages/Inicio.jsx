0259864956b6603cf99e98a412e32ee808a6f524
PedroSilva44 <pedrohss.496@gmail.com>
2025-09-15 18:14:03 -0300

    Add files via upload

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Inicio.scss'
+
+function Inicio() {
+  return (
+    <div className="inicio-container">
+      <h1>Nome 1: Pedro Henrique Da Silva</h1>
+      <h1>Nome 2: Vinícius Fernandes Dos Anjos </h1>
+      <p>Turma: Informática - D</p>
+    </div>
+  )
+}
+
+export default Inicio
